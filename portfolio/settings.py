4c053ece7aec60193bf103b22bd47c7a03732525
Gopika242 <gopikatp712@gmail.com>
2025-02-27 10:32:29 +0530

    settings changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,15 +82,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import os
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
+EMAIL_HOST_USER = 'gopikatp712@gmail.com'  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = 'jyle mpbx rlpi wjek'  # Replace with App Password
 
 
 # Password validation
